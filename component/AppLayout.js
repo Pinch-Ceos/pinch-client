90f9c5c328fed7c86ad61852f8f4b0e2cf475086
GeonHeeAhn <justamoment@ewhain.net>
2021-07-11 00:21:08 +0900

    main page css

--- component/AppLayout.js
+++ component/AppLayout.js
@@ -7,85 +7,134 @@ import {
   TeamOutlined,
   UserOutlined,
 } from '@ant-design/icons';
+import { createGlobalStyle } from 'styled-components';
 
 const { Header, Content, Sider } = Layout;
 const { SubMenu } = Menu;
 
+const Global = createGlobalStyle`
+  .ant-row{
+    margin-right: 0 !important;
+    margin-left: 0 !important;
+  }
+  .ant-col:first-child{
+    padding-left: 0 !important;
+  }
+  .ant-col:last-child{
+    padding-right: 0 !important;
+  }
+`;
+
 const AppLayout = ({ children }) => {
   return (
-    <Layout>
-      <Header
-        className="site-layout-background"
-        style={{ padding: 0, height: 300 }}
-      />
-      <Layout className="site-layout">
-        <Row>
-          <Col xs={24} md={4}>
-            <Sider
-              breakpoint
-              width={{
-                xs: '480px',
-                sm: '576px',
-                md: '768px',
-                lg: '992px',
-                xl: '1200px',
-                xxl: '1600px',
-              }}
-              style={{ ackgroundColor: 'white' }}
-            >
-              <div className="logo" />
-              <Menu
-                theme="dark"
-                defaultSelectedKeys={['1']}
-                mode="inline"
+    <>
+      <Global />
+      <Layout>
+        <Header
+          className="site-layout-background"
+          style={{ padding: 0, height: 200, backgroundColor: 'lightgrey' }}
+        />
+        <Layout
+          className="site-layout"
+          style={{ paddingTop: 10, backgroundColor: 'white' }}
+        >
+          <Row style={{ border: 'none' }}>
+            <Col md={1}></Col>
+            <Col xs={24} md={4}>
+              <Sider
+                breakpoint
+                width={{
+                  xs: '480px',
+                  sm: '576px',
+                  md: '768px',
+                  lg: '992px',
+                  xl: '1200px',
+                  xxl: '1600px',
+                }}
                 style={{ backgroundColor: 'white' }}
               >
-                <Menu.Item
-                  key="1"
-                  icon={<PieChartOutlined />}
-                  style={{ color: 'black' }}
+                <div className="logo" />
+                <Menu
+                  theme="light"
+                  defaultSelectedKeys={['1']}
+                  mode="inline"
+                  style={{
+                    backgroundColor: 'white',
+                    marginLeft: 10,
+                    marginRight: 10,
+                  }}
                 >
-                  Option 1OptionOptionOptionOptionOption
-                </Menu.Item>
-                <Menu.Item
-                  key="2"
-                  icon={<DesktopOutlined />}
-                  style={{ color: 'black' }}
-                >
-                  Option 2
-                </Menu.Item>
-                <Menu.Item
-                  key="sub1"
-                  icon={<UserOutlined />}
-                  style={{ color: 'black' }}
-                >
-                  user
-                </Menu.Item>
-                <SubMenu
-                  key="sub2"
-                  icon={<TeamOutlined />}
-                  title="Team"
-                  style={{ color: 'black' }}
-                >
-                  <Menu.Item key="6">Team 1</Menu.Item>
-                  <Menu.Item key="8">Team 2</Menu.Item>
-                </SubMenu>
-                <Menu.Item
-                  key="9"
-                  icon={<FileOutlined />}
-                  style={{ color: 'black' }}
-                >
-                  Files
-                </Menu.Item>
-              </Menu>
-            </Sider>
-          </Col>
-          <Col xs={24} md={20}>
-            <Content style={{ margin: '0 16px' }}>{children}</Content>
-          </Col>
-        </Row>
+                  <Menu.Item
+                    key="1"
+                    icon="👀"
+                    // icon={<PieChartOutlined />}
+                    style={{
+                      color: 'black',
+                      fontWeight: 'bold',
+                      borderRadius: 10,
+                    }}
+                  >
+                    &nbsp;Inbox
+                  </Menu.Item>
+                  <Menu.Item
+                    key="2"
+                    // icon={<DesktopOutlined />}
+                    icon="📚"
+                    style={{
+                      color: 'black',
+                      fontWeight: 'bold',
+                      borderRadius: 10,
+                    }}
+                  >
+                    &nbsp;구독 중인 뉴스레터
+                  </Menu.Item>
+                  <Menu.Item
+                    key="sub1"
+                    // icon={<UserOutlined />}
+                    icon="📌"
+                    style={{
+                      color: 'black',
+                      fontWeight: 'bold',
+                      borderRadius: 10,
+                    }}
+                  >
+                    &nbsp;저장한 뉴스레터
+                  </Menu.Item>
+                  <SubMenu
+                    key="sub2"
+                    icon={<TeamOutlined />}
+                    title="Team"
+                    style={{
+                      color: 'black',
+                      fontWeight: 'bold',
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Menu.Item key="6">Team 1</Menu.Item>
+                    <Menu.Item key="8">Team 2</Menu.Item>
+                  </SubMenu>
+                  <Menu.Item
+                    key="9"
+                    icon={<FileOutlined />}
+                    style={{
+                      color: 'black',
+                      fontWeight: 'bold',
+                      borderRadius: 10,
+                    }}
+                  >
+                    Files
+                  </Menu.Item>
+                </Menu>
+              </Sider>
+            </Col>
+            <Col xs={24} md={18}>
+              <Content style={{ margin: '0 16px' }}>{children}</Content>
+            </Col>
+            <Col md={1}></Col>
+          </Row>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   );
 };
 export default AppLayout;
--- pages/main.js
+++ pages/main.js
@@ -7,22 +7,25 @@ const Home = () => {
   const cardHeader = '뉴스레터 전체목록';
   const cardData = [
     {
-      src: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+      src: 'https://i.pinimg.com/736x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg',
+      title: 'Card title',
+      description:
+        'asdasdasdasdasdasdxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxasdasasda',
+    },
+    {
+      src: 'https://i.pinimg.com/originals/4b/64/69/4b64690a8654155e6e799031fc2d5a19.jpg',
+      title:
+        'Card titleCard titlCard titlCard titlvCard titlCard titlCard titlCard titlCard titlCard titlCard titlCard titlCard titlCard titl',
+      description:
+        'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    },
+    {
+      src: 'https://1.bp.blogspot.com/-S7hP-_bf7o8/WWZy7CRaT3I/AAAAAAAByHE/wtoIBrfrBCciHVLM5xAopxFVpYIeTN6-ACLcBGAs/s1600/funny-cat-267-01.jpg',
       title: 'Card title',
       description: 'asdasdasdasdasdasdasdasasda',
     },
     {
-      src: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-      title: 'Card title',
-      description: 'asdasdasdasdasdasdasdasasda',
-    },
-    {
-      src: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
-      title: 'Card title',
-      description: 'asdasdasdasdasdasdasdasasda',
-    },
-    {
-      src: 'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+      src: 'https://i.pinimg.com/736x/33/32/6d/33326dcddbf15c56d631e374b62338dc.jpg',
       title: 'Card title',
       description: 'asdasdasdasdasdasdasdasasda',
     },
