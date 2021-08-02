a345c5f2f3c89a81cefe4641e363947484d93957
GeonHeeAhn <justamoment@ewhain.net>
2021-08-03 01:15:25 +0900

    feat: empty inbox page

--- pages/inbox.js
+++ pages/inbox.js
@@ -7,10 +7,12 @@ import AppLayout from '../component/AppLayout';
 import CardList from '../component/CardList';
 import { LOAD_MAIL_REQUEST, LOAD_MY_INFO_REQUEST } from '../reducers';
 import wrapper from '../store/configureStore';
+import styled from 'styled-components';
+import Image from 'next/image';
 
 const Inbox = () => {
   const dispatch = useDispatch();
-  const { mails, hasMoreMails, loadMailLoading } = useSelector(
+  const { mails, hasMoreMails, loadMailLoading, me } = useSelector(
     (state) => state
   );
   const [header, setHeader] = useState('');
@@ -18,7 +20,7 @@ const Inbox = () => {
   const [page, setPage] = useState(2);
 
   useEffect(() => {
-    setHeader('전체 뉴스레터');
+    setHeader(me.user_name.concat('전체 뉴스레터'));
     dispatch({
       type: LOAD_MAIL_REQUEST,
       data: '',
@@ -50,14 +52,31 @@ const Inbox = () => {
     };
   }, [mails.length, hasMoreMails, loadMailLoading]);
 
+  const ChangeBody = () => {
+    if (me.subscription_num === 0 || mails === null) {
+      return (
+        <>
+          <Header>{header}</Header>
+          <EmptyContainer>
+            <Image src={'/design/emptyInbox.png'} width="252" height="263" />
+            <TextLabel>아무것도 없네요!</TextLabel>
+          </EmptyContainer>
+        </>
+      );
+    } else {
+      return <CardList data={mails} header={header} />;
+    }
+  };
+
   return (
     <>
-      <AppLayout>
-        <CardList data={mails} header={header} />
-      </AppLayout>
+      <AppLayout>{ChangeBody()}</AppLayout>
+      {console.log(me.subscription_num)}
+      {console.log(mails)}
     </>
   );
 };
+
 export const getServerSideProps = wrapper.getServerSideProps(
   async (context) => {
     const Token = context.req.headers.cookie.substr(6);
@@ -72,3 +91,29 @@ export const getServerSideProps = wrapper.getServerSideProps(
   }
 );
 export default Inbox;
+
+const EmptyContainer = styled.div`
+  margin-top: 206px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TextLabel = styled.div`
+  font-weight: normal;
+  font-size: 1.5rem;
+  line-height: 1.875rem;
+  text-align: center;
+  color: #999999;
+  margin-bottom: 651px;
+  margin-top: 19px;
+`;
+
+const Header = styled.div`
+  margin-left: 5px;
+  font-weight: bold;
+  font-size: 1.5rem;
+  line-height: 2rem;
+  color: #171920;
+`;
