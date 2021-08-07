0a075ef96d13d489e43b8f827692cc5b27a79244
GeonHeeAhn <justamoment@ewhain.net>
2021-08-08 03:54:08 +0900

    style: remove modal footer

--- component/Modal.js
+++ component/Modal.js
@@ -118,7 +118,6 @@ const ModalWindow = () => {
           boxShadow: '0px 8px 20px rgba(0, 0, 0, 0.25)',
           background: '#2B2E32',
         }}
-        footer={[<div />]}
       >
         <ModalBody />
       </Modal>
